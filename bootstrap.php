9323cc85dd5b8c5d1cde6b48994ac222e7d2a5dd
Evan Mullins <evanrm@gmail.com>
2024-02-14 18:05:15 -0500

    break out into files

---
+++ bootstrap.php
@@ -0,0 +1,40 @@
+<?php
+use NewfoldLabs\WP\ModuleLoader\Container;
+use NewfoldLabs\WP\Module\Context\Context;
+
+/**
+ * Add context to container on init
+ */
+do_action(
+	'init',
+	function () {
+		
+		// Run any registered hooks to set context
+		do_action( 'newfold/context/set' );
+
+		// Add to container
+		add_action( 'newfold_container_set', 
+			function ( Container $container ) {
+
+				// Set all context on container
+				$container->set(
+					'context',
+					Context:all()
+				);
+
+				// TODO: Set up a service ?
+				// $container->set(
+				// 	'contextService',
+				// 	$container->service(
+				// 		function () {
+				// 			return new ContextService();
+				// 		}
+				// 	)
+				// );
+			}
+		);
+
+		// TODO: register API
+		// add_action( 'rest_api_init', array( ContextApi::class, 'registerRoutes' ) );
+	}
+);
