5de5c5633b6c5dccb745480d986b49a8d7be304f
Evan Mullins <evanrm@gmail.com>
2024-02-14 17:54:22 -0500

    move example code to readme - add inline docs

--- includes/Context.php
+++ includes/Context.php
@@ -7,66 +7,121 @@ use function WP_Forge\Helpers\dataGet;
 use function WP_Forge\Helpers\dataSet;
 
 /**
- * This class adds click to buy functionality.
+ * This class adds context functionality.
  **/
 class Context {
 
 	/**
-	 * Dependency injection container.
-	 *
-	 * @var Container
+	 * Context array
 	 */
-	protected $container;
+	public static $context = array();
 
-	public static $context = [];
-
-    public static function all() {
+	/**
+	 * All contexts
+	 * 
+	 * @return Array $context - all context
+	 */
+	public static function all() {
 		return self::$context;
 	}
 
-    public static function get( $name, $default = null ) {
+	/**
+	 * Get the context value
+	 *
+	 * @param String $name - the name of the context to get
+	 * @param String $default - the default value if not defined
+	 * @return Array $context - value of the named context
+	 */
+	public static function get( $name, $default = null ) {
 		return dataGet( self::$context, $name, $default );
 	}
 
+	/**
+	 * Set a context value
+	 *
+	 * @param String $name - the name of the context to set
+	 * @param String $value - the value to set
+	 */
 	public static function set( $name, $value ) {
 		dataSet( self::$context, $name, $value );
 	}
-
-
 }
 
+/**
+ * Helper Get Context Method
+ *
+ * @param String $name - the name of the context to get
+ * @param String $default - the default value if not defined
+ * @return Array $context - value of the named context
+ */
 function getContext( $name, $default = null) {
 	return Context::get( $name, $default );
 }
 
+/**
+ * Helper Set Context Method
+ *
+ * @param String $name - the name of the context to set
+ * @param String $value - the value to set
+ */
 function setContext( $name, $value ) {
 	Context::set( $name, $value );
 }
+
 
 do_action(
 	'init',
 	function () {
 		do_action( 'newfold/context/set' );
-        
-        // TODO: register API
-        // add_action( 'rest_api_init', array( ContextApi::class, 'registerRoutes' ) );
+
+		// Add to container
+		add_action( 'newfold_container_set', 
+			function ( Container $container ) {
+				// Set all context on container
+				$container->set(
+					'context',
+					Context:all()
+				);
+
+				// TODO: Set up a service
+				// $container->set(
+				// 	'contextService',
+				// 	$container->service(
+				// 		function () {
+				// 			return new ContextService();
+				// 		}
+				// 	)
+				// );
+			}
+		);
+
+		// TODO: register API
+		// add_action( 'rest_api_init', array( ContextApi::class, 'registerRoutes' ) );
 	}
 );
 
-add_action(
-	'newfold/context/set',
-	function () {
-		setContext( 'brand.name', 'hostgator' );
-		setContext( 'brand.region', 'BR' );
-	}
-);
-
+/**
+ * This class adds brand helpers to the context with dot notation access.
+ **/
 class Brand {
 
+	/**
+	 * Helper to get the base brand name
+	 */
 	public function base() {
 		return getContext( 'brand.name' );
 	}
+
+	/**
+	 * Helper to get the sub brand name
+	 */
+	public function sub() {
+		return getContext( 'brand.sub' );
+	}
 	
+	/**
+	 * Helper to get the brand region
+	 */
 	public function region() {
 		return getContext( 'brand.region' );
 	}
