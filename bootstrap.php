91298a709f6194aa5bc9dfe50f5de9bc8e579700
Evan Mullins <evanrm@gmail.com>
2024-02-14 18:08:01 -0500

    check wp functions exist, fix `::` typo

--- bootstrap.php
+++ bootstrap.php
@@ -5,36 +5,40 @@ use NewfoldLabs\WP\Module\Context\Context;
 /**
  * Add context to container on init
  */
-do_action(
-	'init',
-	function () {
-		
-		// Run any registered hooks to set context
-		do_action( 'newfold/context/set' );
+if ( function_exists( 'do_action' ) && function_exists( 'add_action' ) ) {
 
-		// Add to container
-		add_action( 'newfold_container_set', 
-			function ( Container $container ) {
+	do_action(
+		'init',
+		function () {
 
-				// Set all context on container
-				$container->set(
-					'context',
-					Context:all()
-				);
+			// Run any registered hooks to set context
+			do_action( 'newfold/context/set' );
 
-				// TODO: Set up a service ?
-				// $container->set(
-				// 	'contextService',
-				// 	$container->service(
-				// 		function () {
-				// 			return new ContextService();
-				// 		}
-				// 	)
-				// );
-			}
-		);
+			// Add to container
+			add_action( 'newfold_container_set', 
+				function ( Container $container ) {
 
-		// TODO: register API
-		// add_action( 'rest_api_init', array( ContextApi::class, 'registerRoutes' ) );
-	}
-);
+					// Set all context on container
+					$container->set(
+						'context',
+						Context::all()
+					);
+
+					// TODO: Set up a service ?
+					// $container->set(
+					// 	'contextService',
+					// 	$container->service(
+					// 		function () {
+					// 			return new ContextService();
+					// 		}
+					// 	)
+					// );
+				}
+			);
+
+			// TODO: register API
+			// add_action( 'rest_api_init', array( ContextApi::class, 'registerRoutes' ) );
+		}
+	);
+
+}
